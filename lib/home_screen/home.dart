b886b6e7f7a72eb839c6061c3bc846a34175a91b
xkhaliil <ltaief.khalil@gmail.com>
2023-04-29 16:13:20 +0100

    home page updated

--- lib/home_screen/home.dart
+++ lib/home_screen/home.dart
@@ -80,7 +80,7 @@ class HomePage extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) =>  ManageCategoriesPage()),
+                      builder: (context) =>  const ManageCategoriesPage()),
                 );
               },
               child: Column(

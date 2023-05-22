784e782309f2def2786e8d55a302c9d00e4f4ba8
wassim.beltaief <wassim.beltaief@wassimbeltaiefs-MacBook-Pro.local>
2023-05-22 23:35:05 +0200

    add form

--- lib/screens/login_screen/login.dart
+++ lib/screens/login_screen/login.dart
@@ -6,6 +6,7 @@ import 'package:pharmacymanagementsystem/salesemployee_screen/emplyeHome.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
   static String routeName = "/Login";
 
   @override
@@ -14,6 +15,7 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen>
     with SingleTickerProviderStateMixin {
+  final _formKey = GlobalKey<FormState>();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   late final AnimationController _controller = AnimationController(
@@ -28,43 +30,46 @@ class _LoginScreenState extends State<LoginScreen>
   }
 
   void signUserIn() async {
-    // show loading circle
-    showDialog(
-      context: context,
-      builder: (context) {
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      },
-    );
+    if (_formKey.currentState!.validate()) {
+      // show loading circle
+      showDialog(
+        context: context,
+        builder: (context) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      );
 
-    // try sign in
-    try {
-      await signInAutomatically();
-      //await signIn();
-      // ignore: unused_catch_clause
-    } on FirebaseAuthException catch (e) {
-      // pop the loading circle
-      Navigator.pop(context);
-      // show error message
-      Lottie.asset("");
-      showErrorMessage();
+      // try sign in
+      try {
+        //await signInAutomatically();
+        await signIn();
+        // ignore: unused_catch_clause
+      } on FirebaseAuthException catch (e) {
+        // pop the loading circle
+        Navigator.pop(context);
+        // show error message
+        Lottie.asset("");
+        showErrorMessage();
+      }
     }
   }
 
-  Future<void> signInAutomatically() async {
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
-      email: "ltaief.khalil@gmail.com",
-      password: "admin111",
-    );
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      Navigator.pop(context);
-      if (FirebaseAuth.instance.currentUser?.email ==
-          "ltaief.khalil@gmail.com") {
-        Navigator.pushNamedAndRemoveUntil(context, HomeScreen.routeName, (routes)=> false);
-      }
-    });
-  }
+  // Future<void> signInAutomatically() async {
+  //   await FirebaseAuth.instance.signInWithEmailAndPassword(
+  //     email: "ltaief.khalil@gmail.com",
+  //     password: "admin111",
+  //   );
+  //   WidgetsBinding.instance.addPostFrameCallback((_) {
+  //     Navigator.pop(context);
+  //     if (FirebaseAuth.instance.currentUser?.email ==
+  //         "ltaief.khalil@gmail.com") {
+  //       Navigator.pushNamedAndRemoveUntil(
+  //           context, HomeScreen.routeName, (routes) => false);
+  //     }
+  //   });
+  // }
 
   Future<void> signIn() async {
     if (emailController.text == "ltaief.khalil@gmail.com") {
@@ -77,7 +82,8 @@ class _LoginScreenState extends State<LoginScreen>
         Navigator.pop(context);
         if (FirebaseAuth.instance.currentUser?.email ==
             "ltaief.khalil@gmail.com") {
-          Navigator.pushNamedAndRemoveUntil(context, HomeScreen.routeName, (routes)=> false);
+          Navigator.pushNamedAndRemoveUntil(
+              context, HomeScreen.routeName, (routes) => false);
         }
       });
     } else {
@@ -87,7 +93,8 @@ class _LoginScreenState extends State<LoginScreen>
       );
       WidgetsBinding.instance.addPostFrameCallback((_) {
         Navigator.pop(context);
-        Navigator.pushNamedAndRemoveUntil(context, EmployeeHomeScreen.routeName, (routes)=> false);
+        Navigator.pushNamedAndRemoveUntil(
+            context, EmployeeHomeScreen.routeName, (routes) => false);
       });
     }
   }
@@ -115,6 +122,7 @@ class _LoginScreenState extends State<LoginScreen>
   }
 
   bool _isObscure = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -143,118 +151,136 @@ class _LoginScreenState extends State<LoginScreen>
             child: SizedBox(
               height: 700,
               width: 500,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Lottie.asset(
-                    'video/login.json',
-                    controller: _controller,
-                    height: 250,
-                    width: 250,
-                    repeat: false,
-                  ),
-                  const SizedBox(height: 25),
-                  const Text(
-                    'Welcome',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 36,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Lottie.asset(
+                      'video/login.json',
+                      controller: _controller,
+                      height: 250,
+                      width: 250,
+                      repeat: false,
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  const SizedBox(
-                    width: 300,
-                    child: Text(
-                      'Efficient, user-friendly, and at your service! Welcome to your one-stop solution for pharmacy management.',
-                      textAlign: TextAlign.center,
+                    const SizedBox(height: 25),
+                    const Text(
+                      'Welcome',
                       style: TextStyle(
-                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 36,
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 50),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                    child: TextField(
-                      controller: emailController,
-                      decoration: InputDecoration(
-                        hintText: 'Email',
-                        prefixIcon: const Icon(Icons.email_outlined,
-                            color: Color(0xFF2C3333)),
-                        filled: true,
-                        fillColor: Color(0xFFE7F6F2),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: BorderSide.none,
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: BorderSide(color: Color(0xFF2C3333)),
-                        ),
-                        contentPadding: const EdgeInsets.symmetric(
-                            vertical: 15, horizontal: 20),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                    child: TextField(
-                      controller: passwordController,
-                      obscureText: _isObscure,
-                      decoration: InputDecoration(
-                        hintText: 'Password',
-                        prefixIcon: const Icon(Icons.lock_outlined,
-                            color: Color(0xFF2C3333)),
-                        filled: true,
-                        fillColor: Color(0xFFE7F6F2),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: BorderSide.none,
-                        ),
-                        suffixIcon: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              _isObscure = !_isObscure;
-                            });
-                          },
-                          icon: Icon(
-                            _isObscure
-                                ? Icons.visibility_off
-                                : Icons.visibility,
-                            color: Color(0xFF2C3333),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  SizedBox(
-                    width: 300,
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: signUserIn,
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            const Color(0xFF434242)),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                        ),
-                      ),
-                      child: const Text(
-                        'Sign In',
+                    const SizedBox(height: 10),
+                    const SizedBox(
+                      width: 300,
+                      child: Text(
+                        'Efficient, user-friendly, and at your service! Welcome to your one-stop solution for pharmacy management.',
+                        textAlign: TextAlign.center,
                         style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
+                          fontSize: 15,
                         ),
                       ),
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 50),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: TextFormField(
+                        controller: emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your email';
+                          } else if (!value.contains('@')) {
+                            return 'Please enter a valid email address';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          prefixIcon: const Icon(Icons.email_outlined,
+                              color: Color(0xFF2C3333)),
+                          filled: true,
+                          fillColor: Color(0xFFE7F6F2),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide.none,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide(color: Color(0xFF2C3333)),
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                              vertical: 15, horizontal: 20),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: TextFormField(
+                        controller: passwordController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a password';
+                          }
+                          return null;
+                        },
+                        obscureText: _isObscure,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          prefixIcon: const Icon(Icons.lock_outlined,
+                              color: Color(0xFF2C3333)),
+                          filled: true,
+                          fillColor: Color(0xFFE7F6F2),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide.none,
+                          ),
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                _isObscure = !_isObscure;
+                              });
+                            },
+                            icon: Icon(
+                              _isObscure
+                                  ? Icons.visibility_off
+                                  : Icons.visibility,
+                              color: Color(0xFF2C3333),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      width: 300,
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: signUserIn,
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              const Color(0xFF434242)),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                        ),
+                        child: const Text(
+                          'Sign In',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
